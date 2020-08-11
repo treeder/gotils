de10ef6c2e51390a12cfbd1f35c49c9fb75e0abb
Travis Reeder <treeder@gmail.com>
2020-08-10 17:29:11 -0700

    Updated contextual errors docs in README

--- log_example/example.go
+++ log_example/example.go
@@ -4,29 +4,41 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"runtime"
 
 	"github.com/treeder/gotils"
 )
 
 func main() {
 	ctx := context.Background()
-	err := errors.New("error 1")
-	fmt.Println(gotils.ErrString(err))
-	err = gotils.Errorf(ctx, "error 2: %v", err)
-	fmt.Println(gotils.ErrString(err))
 	ctx = gotils.With(ctx, "abc", 123)
-	fmt.Println("fields:", gotils.Fields(ctx))
-	err = gotils.C(ctx).Errorf("error3: %v", err)
-	fmt.Println(gotils.ErrString(err))
-
-	// show a regular stack trace too
-	yo()
+	// we got an error from somewhere:
+	err := errors.New("something bad")
+	// fmt.Println(gotils.ErrString(err))
+	err = gotils.Errorf(ctx, "uh oh: %v", err)
+	// fmt.Println(gotils.ErrString(err))
+	// fmt.Println("fields:", gotils.Fields(ctx))
+	err = gotils.C(ctx).Errorf("doh: %v", err)
+	fmt.Print(gotils.ErrString(err))
 }
 
-func yo() {
-	b := make([]byte, 1<<20)
-	runtime.Stack(b, false)
-	fmt.Println("Regular stack")
-	fmt.Println(string(b))
-}
+// func main() {
+// 	ctx := context.Background()
+// 	err := errors.New("error 1")
+// 	fmt.Println(gotils.ErrString(err))
+// 	err = gotils.Errorf(ctx, "error 2: %v", err)
+// 	fmt.Println(gotils.ErrString(err))
+// 	ctx = gotils.With(ctx, "abc", 123)
+// 	fmt.Println("fields:", gotils.Fields(ctx))
+// 	err = gotils.C(ctx).Errorf("error3: %v", err)
+// 	fmt.Println(gotils.ErrString(err))
+
+// 	// show a regular stack trace too
+// 	yo()
+// }
+
+// func yo() {
+// 	b := make([]byte, 1<<20)
+// 	runtime.Stack(b, false)
+// 	fmt.Println("Regular stack")
+// 	fmt.Println(string(b))
+// }
