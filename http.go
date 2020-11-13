5e17c66cb31dcc81904d12c0432a2aa3b25c3d88
Travis Reeder <treeder@gmail.com>
2020-11-13 09:56:24 -0500

    wip

--- http.go
+++ http.go
@@ -212,6 +212,8 @@ func checkError(resp *http.Response) error {
 		// attempt to parse JSON
 		er := &ErrorResponse{}
 		err2 := ParseJSONBytes(bodyBytes, er)
+		fmt.Println("err2:", err2)
+		fmt.Printf("er: %+v\n", er)
 		if err2 != nil {
 			if er.Error.Message != "" {
 				return NewHTTPError(er.Error.Message, resp.StatusCode)
